c919b623dec3b40f0adfa7cec5e4230bb1363669
vinu8468 <vineethdevadiga@gmail.com>
2026-05-03 07:45:52 +0000

    loading both amp and non -amp into a single dataframe(csv)

---
+++ src/load_final_data.py
@@ -0,0 +1,63 @@
+from Bio import SeqIO
+import pandas as pd
+
+
+def load_fasta(file_path,label,min_len=10,max_len=50):
+    sequences = []
+    for record in SeqIO.parse(file_path,"fasta"):
+        seq = str(record.seq)
+        length = len(seq)
+
+        # filtering by length
+        if min_len <= length <= max_len:
+            sequences.append({
+                "id": record.id,
+                "sequence": seq,
+                "label": label,
+                "length": length
+            })
+    return pd.DataFrame(sequences)
+
+
+def main():
+    plant_file = "../data/raw/plantAMPs_APD2024.fasta"
+    uniprot_file = "../data/raw/uniprot_mini.fasta"
+
+    print("Loading AMP dataset...")
+    amp_df = load_fasta(plant_file,label=1)
+    print("Loading non-AMP dataset...")
+    non_amp_df = load_fasta(uniprot_file,label=0)
+
+    # Optional: remove toxin-like sequences (basic filter)
+    non_amp_df = non_amp_df[
+        ~non_amp_df["id"].str.lower().str.contains("toxin", na=False)
+    ]
+    print("Before balancing:")
+    print("AMP count:", len(amp_df))
+    print("Non-AMP count:", len(non_amp_df))
+
+    min_size = min(len(amp_df), len(non_amp_df))
+
+    amp_df = amp_df.sample(min_size, random_state=42)
+    non_amp_df = non_amp_df.sample(min_size, random_state=42)
+
+    # Combine
+    df = pd.concat([amp_df, non_amp_df], ignore_index=True)
+
+    print("\nAfter balancing:")
+    print(df["label"].value_counts())
+    
+    # save the dataset
+    output_path = "../data/processed/final_dataset.csv"
+    df.to_csv(output_path, index=False)
+
+    print("Dataset saved to:", output_path)
+
+    # -----------------------------
+    # BASIC STATS
+    # -----------------------------
+    print("\nLength statistics:")
+    print(df.groupby("label")["length"].describe())
+
+if __name__ == "__main__":
+    main()
